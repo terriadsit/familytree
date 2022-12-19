23220d7fcce2140b8e408d06a8b4945b59627f65
Terri Adsit <terriadsit@yahoo.com>
2022-12-19 12:40:50 -0600

    Update CheckPerson.cy.js

--- cypress/e2e/CheckPerson.cy.js
+++ cypress/e2e/CheckPerson.cy.js
@@ -18,7 +18,7 @@ describe('CheckPerson reveals possible duplicates when using AddPerson Name fiel
     cy.get('[cy-test-id=duplicate-heading]').should('be.visible')
   })
 
-  it('doe not display if not a possible duplicate', () => {
+  it('does not display if not a possible duplicate', () => {
     cy.get('[cy-test-id=name]').clear().type('1234567 ')
     cy.get('[cy-test-id=duplicate-heading]').should('not.exist')
   })
